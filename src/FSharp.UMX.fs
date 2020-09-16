2ad376bfbbd9b824c814fbbf3f2983a056c980df
Peder Sørensen <pks1986@gmail.com>
2020-09-16 08:56:38 +0200

    support adding and subtracting measured values

--- src/FSharp.UMX.fs
+++ src/FSharp.UMX.fs
@@ -68,6 +68,29 @@ type UMX =
     static member inline cast<[<Measure>]'m1, [<Measure>]'m2> (x : DateTime<'m1>) : DateTime<'m2> = Unsafe.cast x
     static member inline cast<[<Measure>]'m1, [<Measure>]'m2> (x : DateTimeOffset<'m1>) : DateTimeOffset<'m2> = Unsafe.cast x
 
+    static member inline add<[<Measure>]'m> (t1: byte<'m>, t2: byte<'m>) : byte<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: int<'m>, t2: int<'m>) : int<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: int16<'m>, t2: int16<'m>) : int16<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: int64<'m>, t2: int64<'m>) : int64<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: uint64<'m>, t2: uint64<'m>) : uint64<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: float<'m>, t2: float<'m>) : float<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: decimal<'m>, t2: decimal<'m>) : decimal<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: string<'m>, t2: string<'m>) : string<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: TimeSpan<'m>, t2: TimeSpan<'m>) : TimeSpan<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: DateTime<'m>, t2: TimeSpan<'m>) : DateTime<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+    static member inline add<[<Measure>]'m> (t1: DateTimeOffset<'m>, t2: TimeSpan<'m>) : DateTimeOffset<'m> = UMX.tag(UMX.untag t1 + UMX.untag t2)
+
+    static member inline subtract<[<Measure>]'m> (t1: byte<'m>, t2: byte<'m>) : byte<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: int<'m>, t2: int<'m>) : int<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: int16<'m>, t2: int16<'m>) : int16<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: int64<'m>, t2: int64<'m>) : int64<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: uint64<'m>, t2: uint64<'m>) : uint64<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: float<'m>, t2: float<'m>) : float<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: decimal<'m>, t2: decimal<'m>) : decimal<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: DateTime<'m>, t2: DateTime<'m>) : TimeSpan<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: DateTime<'m>, t2: TimeSpan<'m>) : DateTime<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: DateTimeOffset<'m>, t2: DateTimeOffset<'m>) : TimeSpan<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
+    static member inline subtract<[<Measure>]'m> (t1: DateTimeOffset<'m>, t2: TimeSpan<'m>) : DateTimeOffset<'m> = UMX.tag(UMX.untag t1 - UMX.untag t2)
 
 [<AutoOpen>]
 module Operators =
@@ -75,6 +98,20 @@ module Operators =
   let inline private _cast< ^TC, ^xm, ^xn when (^TC or ^xm or ^xn) : (static member cast : ^xm -> ^xn)> (x : ^xm) =
       ((^TC or ^xm or ^xn) : (static member cast : ^xm -> ^xn) x)
 
+  let inline private _add< ^TC, ^xm, ^xn, ^xo when (^TC or ^xm or ^xn or ^xo) : (static member add : ^xm -> ^xn -> ^xo)>
+      (x : ^xm) (y : ^xn) =
+      ((^TC or ^xm or ^xn or ^xo) : (static member add : ^xm -> ^xn -> ^xo) (x, y))
+
+  let inline private _subtract< ^TC, ^xm, ^xn, ^xo when (^TC or ^xm or ^xn or ^xo) : (static member subtract : ^xm -> ^xn -> ^xo)>
+      (x : ^xm) (y : ^xn) =
+      ((^TC or ^xm or ^xn or ^xo) : (static member subtract : ^xm -> ^xn -> ^xo) (x, y))
+
   // NB the particular infix operator shadows the rarely used quotation splicing operator
   /// Infix operator used for tagging, untagging, or casting units of measure
   let inline (~%) (x : ^xm) : ^xn = _cast<UMX, ^xm, ^xn> x
+
+  /// Infix operator used for adding values with compatible units of measure
+  let inline (+) (x : ^xm) (y : ^xn) : ^xo = _add<UMX, ^xm, ^xn, ^xo> x y
+
+  /// Infix operator used for subtracting values with compatible units of measure
+  let inline (-) (x : ^xm) (y : ^xn) : ^xo = _subtract<UMX, ^xm, ^xn, ^xo> x y
--- tests/FSharp.UMX.Tests.fs
+++ tests/FSharp.UMX.Tests.fs
@@ -36,6 +36,38 @@ let ``Simple unit of measure conversions with cast operator``() =
     ()
 
 [<Fact>]
+let ``Simplt unit of measure operators``() =
+    let d1 : DateTime<foo> = % DateTime.Today
+    let d2 : DateTime<foo> = % DateTime.Today
+    let t0 : TimeSpan<foo> = %TimeSpan.Zero
+    let t1 = d2 - d1
+    let d2 = d1 + t1
+    let d3 = t0 + t1
+    let d4 = d1 - t0
+    let o1 : DateTimeOffset<foo> = %DateTimeOffset.Now
+    let o2 : DateTimeOffset<foo> = %DateTimeOffset.Now
+    let t2 = o2 - o1
+    let o3 = o1 + t2
+    let o4 = d1 - t0
+    let z : int<km> = % 42
+    let z' = 42<km>
+    let z'' = z - z'
+    let z''' = z + z'
+    let b0 : byte<foo> = %3uy
+    let b1 : byte<foo> = %4uy
+    let b3 = b0 - b1
+
+    let s1 = %"hello"
+    let s2 = %" umx"
+    let s3 = s1 + s2
+
+    let b1 : bool<foo> = %true
+    let b2 : bool<foo> = %true
+    let b3 : bool<foo> = %(%b1 && %b2)
+
+    ()
+
+[<Fact>]
 let ``Simple unit of measure conversions with UMX.tag function``() =
     let x = UMX.tag<skuId> (Guid.NewGuid())              
     let y = UMX.tag<skuId> ((%x).ToString())             
